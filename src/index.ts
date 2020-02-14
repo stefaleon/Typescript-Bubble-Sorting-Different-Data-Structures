b72da4c17732c58cf83d2441ef438096f5bd68b3
stefaleon <stefaleon@gmail.com>
2020-02-14 16:19:01 +0200

    bubble sorting array elements

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("bubble sort");
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([5, 7, 0, -2, -22, 1]);
+sorter.sort();
+console.log(sorter.collection);

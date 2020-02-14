acb6003e28f75aaea12fee22f17a92a1f589e1f1
stefaleon <stefaleon@gmail.com>
2020-02-14 23:35:30 +0200

    configure the LinkedListOfNumbers class and use it to sort list data

---
+++ src/LinkedLIstOfNumbers.ts
@@ -0,0 +1,78 @@
+class NodeOfList {
+  next: NodeOfList | null = null;
+
+  constructor(public data: number) {}
+}
+
+export class LinkedListOfNumbers {
+  head: NodeOfList | null = null;
+
+  add(data: number): void {
+    const node = new NodeOfList(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+    return length;
+  }
+
+  at(index: number): NodeOfList {
+    if (!this.head) {
+      throw new Error("Index out of bounds");
+    }
+    let counter = 0;
+    let node: NodeOfList | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    throw new Error("Index out of bounds");
+  }
+
+  leftIsBigger(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const temp = this.at(leftIndex).data;
+    this.at(leftIndex).data = this.at(rightIndex).data;
+    this.at(rightIndex).data = temp;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+    let node: NodeOfList | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { ArrayOfNumbers } from "./ArrayOfNumbers";
 import { StringOfCharacters } from "./StringOfCharacters";
+import { LinkedListOfNumbers } from "./LinkedLIstOfNumbers";
 
 const arrayOfNumbersSample = new ArrayOfNumbers([501, 7, 0, -2.32, -22, 1]);
 const arrSorter = new Sorter(arrayOfNumbersSample);
@@ -13,3 +14,16 @@ const stringOfCharsSample = new StringOfCharacters(
 const stringSorter = new Sorter(stringOfCharsSample);
 stringSorter.sort();
 console.log(stringSorter.collection);
+
+const listSample = new LinkedListOfNumbers();
+listSample.add(100);
+listSample.add(42);
+listSample.add(-10);
+listSample.add(13);
+listSample.add(77);
+console.log("list before sorting:");
+listSample.print();
+const listSorter = new Sorter(listSample);
+listSorter.sort();
+console.log("list after sorting:");
+listSample.print();

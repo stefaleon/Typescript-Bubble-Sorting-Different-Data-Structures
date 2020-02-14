b114d117e41e354a1246cf02291cadebb77c53aa
stefaleon <stefaleon@gmail.com>
2020-02-14 17:40:03 +0200

    type guard for array of numbers

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
         }
       }
     }
